a6b82495c14cff2a9da80f3a62e3c266f2f2f5c5
Jay Lin <ljlovemichael@hotmail.com>
2018-10-25 10:01:41 +0800

    //66 add comment

--- Solution_66.hpp
+++ Solution_66.hpp
@@ -1,5 +1,16 @@
 #ifndef SOLUTION_66
 #define SOLUTION_66
+/* Given a non-empty array of digits representing a non-negative integer,
+plus one to the integer.
+The digits are stored such that the most significant digit is at the head of the list,
+and each element in the array contain a single digit.
+You may assume the integer does not contain any leading zero,
+except the number 0 itself.
+对一个vector<int>代表的长整数+1
+处理好进位，最高位会不会进位
+可以再初始化一个vector
+或者在digits上直接操作
+*/
 #include <vector>
 class Solution_66 {
  public:
