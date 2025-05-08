4a025959d52105e055f3cc359440f83450c20b8d
Jay Lin <ljlovemichael@hotmail.com>
2025-05-08 21:32:49 +0800

    2824 Count Pairs Whose Sum is Less than Target

---
+++ Search/Solution_2824.hpp
@@ -0,0 +1,33 @@
+
+#ifndef SOLUTION_2824
+#define SOLUTION_2824
+#include <algorithm>
+#include <vector>
+/*
+Given a 0-indexed integer array nums of length n and an integer target,
+return the number of pairs (i, j) 
+where 0 <= i < j < n and 
+nums[i] + nums[j] < target.
+
+sort the array at first.
+calculate the sum of left and right position.
+
+*/
+class Solution_2824 {
+public:
+  int countPairs(std::vector<int> &nums, int target) {
+    int result = 0;
+    std::sort(nums.begin(), nums.end());
+    int left = 0, right = nums.size() - 1;
+    while (left < right) {
+      if (nums[left] + nums[right] < target) {
+        result += (right - left);
+        left++;
+      } else {
+        right--;
+      }
+    }
+    return result;
+  }
+};
+#endif
