1149b2632a1800299707d776d9ff774f3b45c61b
Jay Lin <ljlovemichael@hotmail.com>
2025-07-15 20:27:23 +0800

    42 add explanation

--- Array/Solution_42.hpp
+++ Array/Solution_42.hpp
@@ -73,28 +73,46 @@ class Solution_42_bak {
     return count;
   }
 };
+/*
+柱子高度最低为 0.
+取左右各一根柱子，中间相隔若干根柱子。
+两根柱子之间能够储水意味着这两个柱子之间的柱子中有比这两根柱子更低的柱子。
+如果右边的柱子相对左边柱子更高，
+那么左边柱子到右边柱子之间，只要有比左边柱子低的柱子，就能在这些较低柱子处储水。
+
+双指针方法，从两边向内侧统计储水总量。
+左右两侧预设的最高高度为 0.
+先获得左右两侧高度，获得的两个高度分别与已知的左右两侧最大高度比较，更新两侧最大高度。
+如果右侧的最大高度更高，那么左侧就可以储水。
+  （只要左侧的柱子高度比左边最大高度低，相同时储水为 0，不影响逻辑）
+  即便这个位置右边可能存在更高的柱子，但这个位置储水量只与其左边所有柱子高度最大值有关，
+  右边无论有多高的柱子都不能使这个位置多储水，这是短板原理。
+  这个位置储水量就是（左边最高柱子高度-这个位置柱子高度）*（长度 = 1）
+  这个位置计算过之后就可以往里走。继续统计
+如果左侧的最大高度更高，那么右侧就可以储水。
+直到左右相遇结束统计。
+*/
 class Solution_42_2 {
  public:
   int trap(std::vector<int> &height) {
+    int i = 0, j = height.size() - 1;
     int result = 0;
-    int size = height.size();
-    int i = 1, j = size - 2;
-    int left_max = height[0];
-    int right_max = height[size - 1];
-    while (i <= j) {
+    int left_most = 0;
+    int right_most = 0;
+    while (i < j) {
       int left = height[i];
       int right = height[j];
-      left_max = std::max(left, left_max);
-      right_max = std::max(right, right_max);
-      int water = 0;
-      if (left_max < right_max) {
-        water = std::min(left_max, right_max) - left;
-        i++;
-      } else {
-        water = std::min(left_max, right_max) - right;
+      left_most = std::max(left_most, left);
+      right_most = std::max(right_most, right);
+      int vol = 0;
+      if (left_most > right_most) {
+        vol = right_most - right;
         j--;
+      } else {
+        vol = left_most - left;
+        i++;
       }
-      result += water;
+      result += vol;
     }
     return result;
   }
